5d5810dd788affdb814ef81fc32c4135a018fd94
silviolimm-spec <silviokerennn@gmail.com>
2026-04-07 13:34:47 +0700

    feat: implement student index page

--- app/controllers/studentcontroller.php
+++ app/controllers/studentcontroller.php
@@ -6,20 +6,17 @@ class StudentController
 
     public function index(): void
     {
-        echo '<h1>Daftar Siswa</h1>';
-            echo '<p>Menampilkan Daftar Siswa</p>';
+        require_once '../app/views/students/index.php';
     }
 
     public function create(): void
     {
-        echo '<h1>Tambah Siswa</h1>';
-        echo '<p>Menampilkan Form Tambah Siswa</p>';
+        require_once '../app/views/students/create.php';
     }
 
     public function show(string $id):void
     {
-        echo'<h1> Detail Siswa </h1>';
-        echo"<p> Menampilkan detail siswa ID: {$id} </p>";
+        require_once '../app/views/students/show.php';
     }
 }
 ?>
--- app/core/Router.php
+++ app/core/Router.php
@@ -33,7 +33,7 @@ class Router
 
             if (preg_match($pattern, $uri, $matches)) {
                 array_shift($matches);
-                require_once './app/controllers/' . $route['controller'] . '.php';
+                require_once '../app/controllers/' . $route['controller'] . '.php';
     
                 $controllerClass = 'App\\Controllers\\' . $route['controller'];
                 $controller = new $controllerClass();
@@ -52,5 +52,3 @@ class Router
 }
 
 ?>
-
-ini router.php
